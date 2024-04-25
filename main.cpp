7a95917d328cc415b6da3b47509e90527a4ddce8
karasb1 <bohdankaras456@gmail.com>
2024-04-25 19:31:50 +0300

    The ex4 was done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-
+	SetConsoleOutputCP(1251);
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	float value1 = 0;
+	float value2 = 0;
+	cout << "Обчислення відстані між населеними пунктами." << endl;
+	cout << "Введіть вихідні дані:" << endl;
+	cout << "Масштаб карти (кількість кілометрів в одному сантиметрі) -> ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cin >> value1;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Відстань між точками, що зображують населені пункти (см) -> ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cin >> value2;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Відстань між населеними пунктами ";
+	cout << value1 * value2 << " км" << endl;
 	return 0;
 }
