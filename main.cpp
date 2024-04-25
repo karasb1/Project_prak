6c12c7a005dcda1771ee39bf6337a391dd1898a5
karasb1 <bohdankaras456@gmail.com>
2024-04-25 20:12:19 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-
+	int hr, kop;
+	cout << "Enter hryvnias: ";
+	cin >> hr;
+	cout << "Enter kopecks: ";
+	cin >> kop;
+	hr += kop / 100;
+	kop %= 100;
+	cout << "hryvnias: " << hr << endl;
+	cout << "kopecks: " << kop << endl;
 	return 0;
 }
