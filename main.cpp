abfe7d8272d1be3ccf58af94c4804ad138dc4b95
karasb1 <bohdankaras456@gmail.com>
2024-04-25 19:46:02 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-
+	SetConsoleOutputCP(1251);
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	float value1 = 0;
+	float value2 = 0;
+	float value3 = 0;
+	cout << "Обчислення об'єму пралелепіпеда." << endl;
+	cout << "Введіть вихідні дані:" << endl;
+	cout << "Довжина (см) -> ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cin >> value1;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Ширина (см) -> ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cin >> value2;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Висота (см) -> ";
+	SetConsoleTextAttribute(hConsole, 0x05);
+	cin >> value3;
+	SetConsoleTextAttribute(hConsole, 0x07);
+	cout << "Об'єм: ";
+	cout << value1 * value2 * value3 << " куб.см." << endl;
 	return 0;
 }
