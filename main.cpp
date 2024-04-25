80da277b7b1a9761231c545395e5a6678975d797
karasb1 <bohdankaras456@gmail.com>
2024-04-25 20:18:53 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
-
+	int year;
+	cout << "Enter the year: ";
+	cin >> year;
+	bool res = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+	cout << "In this year " << 365 + res << " days" << endl;
 	return 0;
 }
