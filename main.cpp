109916ec2ed490c16e648363decf69a1e1b58105
karasb1 <bohdankaras456@gmail.com>
2024-04-25 19:19:34 +0300

    ex5 was done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
-
+	float volume = 0;
+	float res = 0;
+	cout << "Enter the volume of the ball: " << endl;
+	cin >> volume;
+	res = (4* 3.14*pow(volume,3))/3;
+	cout << res << endl;
 	return 0;
 }
