70f2402c1bb12ee6f00ef3ffa8795fe8b311d293
karasb1 <bohdankaras456@gmail.com>
2024-04-27 11:29:58 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+	int seconds, minutes, hours;
+	cin >> seconds;
+	cout << seconds/2;
+	cout << "hours: " << seconds / 3600 << endl;
+	cout << "minutes: " << seconds % 3600 / 60 << endl;
+	cout << "seconds: " << seconds % 3600 % 60 << endl;
 
 	return 0;
 }
