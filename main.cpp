cb1d103186cb10e511e12463a8ed910fc0651c4e
karasb1 <bohdankaras456@gmail.com>
2024-04-27 11:44:49 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double a;
+	cout << "" << endl;
+	cin >> a;
+	double grn = static_cast<int>(a);
+	double cop = static_cast<int>((a - grn) * 100);
+	cout << grn << " грн. " << cop << " коп.";
 
 	return 0;
 }
